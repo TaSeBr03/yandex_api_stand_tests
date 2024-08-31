e325c69368fc06610711c963ff9315e89328a81c
Tatyana Bronman <taceta@yandex.ru>
2024-08-31 15:49:04 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://8b1880ac-2eb6-4eca-b2be-922be63914f3.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
---
+++ sender_stand_request.py
@@ -0,0 +1,22 @@
+import requests
+import configuration
+import data
+
+def get_docs():
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+#response = get_docs()
+#print(response.status_code)
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH,params={"count": 20})
+#response = get_logs()
+#print(response.status_code)
+#print(response.headers)
+def get_user_table():
+    return requests.post(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+response = get_user_table()
+print(response.status_code)
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,json=body,headers=data.headers)
+#response = post_new_user(data.user_body)
+#print(response.status_code)
+#print(response.json())
